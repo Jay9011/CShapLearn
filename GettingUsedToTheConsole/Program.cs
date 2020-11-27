04c2ad24eb928e810179306e4b4b11e574657184
Jay9011 <whdtjq5904@gmail.com>
2020-11-28 01:52:35 +0900

    기본 자료형과 var, dynamic

---
+++ GettingUsedToTheConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GettingUsedToTheConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            byte byteNumber;
+            short shortNumber;
+            int integerNumber;
+            long longNumber;
+            Int16 _16bitNumber; // short    (.net)
+            Int32 _32bitNumber; // int      (.net)
+            Int64 _64bitNumber; // long     (.net)
+
+            float floatNumebr;      // 4byte
+            double doubleNumber;    // 8byte
+            decimal decimalNumber;  // 16byte
+
+            char character; // 16bit Unicode
+            string characterString; // unicode
+
+            var variable = 15; // Type inference
+            dynamic dynamicLanguageRuntime; // like Ruby, Python
+
+            Console.WriteLine(variable + " is variable Type");
+        }
+    }
+}
