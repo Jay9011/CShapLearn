4880864128b988b0efd73e9b3a9f098b63349023
Jay9011 <whdtjq5904@gmail.com>
2020-11-29 01:56:00 +0900

    라이브러리 사용

---
+++ TestLibraryConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TestLibrary;
+
+namespace TestLibraryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new FirstClass();
+            first.Hello();
+        }
+    }
+}
