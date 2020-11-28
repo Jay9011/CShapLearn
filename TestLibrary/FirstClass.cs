0763968a4987e46e1bce722c03d86b1751386ad8
Jay9011 <whdtjq5904@gmail.com>
2020-11-29 01:55:51 +0900

    라이브러리 생성

---
+++ TestLibrary/FirstClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLibrary
+{
+    public class FirstClass
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello First Library");
+        }
+    }
+}
