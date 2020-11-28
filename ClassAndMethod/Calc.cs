fe798989fe37f29d955cbb333af919c2a8a95301
Jay9011 <whdtjq5904@gmail.com>
2020-11-29 01:55:32 +0900

    클래스와 제네릭 메소드

---
+++ ClassAndMethod/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndMethod
+{
+    class Calc
+    {
+        public decimal? Plus<T>(T num1, T num2)
+        {
+            return (num1 as decimal?) + (num2 as decimal?);
+        }
+    }
+}
---
+++ ClassAndMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+
+            Console.Write("num1 : ");
+            var num1 = Console.ReadLine();
+            decimal number1 = Decimal.Parse(num1);
+            Console.Write("num2 : ");
+            var num2 = Console.ReadLine();
+            decimal number2 = Decimal.Parse(num2);
+
+            Console.WriteLine(calc.Plus(number1, number2));
+        }
+    }
+}
