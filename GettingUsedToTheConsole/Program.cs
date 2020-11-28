3f42dc6840354d8992958f3b4f0c9222bc938f63
Jay9011 <whdtjq5904@gmail.com>
2020-11-29 00:42:15 +0900

    기본 자료형과 var, dynamic

--- GettingUsedToTheConsole/Program.cs
+++ GettingUsedToTheConsole/Program.cs
@@ -7,13 +7,13 @@ namespace GettingUsedToTheConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            byte byteNumber;
-            short shortNumber;
-            int integerNumber;
-            long longNumber;
-            Int16 _16bitNumber; // short    (.net)
-            Int32 _32bitNumber; // int      (.net)
-            Int64 _64bitNumber; // long     (.net)
+            byte byteNumber = 127;
+            short shortNumber = 32767;
+            int integerNumber = 2147483647;
+            long longNumber = 9223372036854775807;
+            Int16 _16bitNumber = 32767; // short    (.net)
+            Int32 _32bitNumber = 2147483647; // int      (.net)
+            Int64 _64bitNumber = 9223372036854775807; // long     (.net)
 
             float floatNumebr;      // 4byte
             double doubleNumber;    // 8byte
@@ -26,6 +26,30 @@ namespace GettingUsedToTheConsole
             dynamic dynamicLanguageRuntime; // like Ruby, Python
 
             Console.WriteLine(variable + " is variable Type");
+
+            Console.Write("input value : ");
+
+            var input = Console.ReadLine();
+            if (input == variable.ToString())
+            {
+                Console.WriteLine("same value");
+            }
+            else
+            {
+                Console.WriteLine("different value");
+                var num = 1;
+
+                while (true)
+                {
+                    Console.Write("{0,3}", num++);
+                    if (variable == num)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("variable is {0}", num);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
