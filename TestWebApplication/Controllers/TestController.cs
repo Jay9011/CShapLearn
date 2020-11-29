22fc9911fa6792502a087eb498d4401fea73e74d
Jay9011 <whdtjq5904@gmail.com>
2020-11-29 20:07:54 +0900

    Controller/View 와 Razor Syntax

---
+++ TestWebApplication/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Study()
+        {
+            return View();
+        }
+    }
+}
